be0efd37173b0f8826c16db415fcfb03f0431208
HyoJong-Moon <gywhd408@naver.com>
2020-07-22 16:31:44 +0900

    add spout, bolt, topology of version4

---
+++ src/main/java/storm/v4/level0/LSTMBolt.java
@@ -0,0 +1,56 @@
+package storm.v4.level0;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseRichBolt;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Values;
+import org.tensorflow.SavedModelBundle;
+import org.tensorflow.Session;
+import org.tensorflow.Tensor;
+import storm.v4.input.Preprocessor;
+
+import java.util.Map;
+
+public class LSTMBolt extends BaseRichBolt {
+    private Log log = LogFactory.getLog(LSTMBolt.class);
+    private OutputCollector outputCollector;
+    private Preprocessor preprocessor;
+    private SavedModelBundle savedModelBundle;
+    private Session sess;
+
+    @Override
+    public void prepare(Map map, TopologyContext topologyContext, OutputCollector outputCollector) {
+        this.outputCollector = outputCollector;
+        this.preprocessor = new Preprocessor();
+        this.savedModelBundle = SavedModelBundle.load("/home/hjmoon/models/url/","serve");
+        this.sess = savedModelBundle.session();
+    }
+
+    @Override
+    public void execute(Tuple tuple) {
+        String url = tuple.getStringByField("url");
+        int[][] input = preprocessor.convert(url);
+
+        Tensor x = Tensor.create(input);
+        Tensor result = sess.runner()
+                .feed("lstm_input:0", x)
+                .fetch("lstm_output/Sigmoid:0")
+                .run()
+                .get(0);
+
+        float[][] pred = (float[][]) result.copyTo(new float[1][1]);
+
+        outputCollector.emit(new Values(url, tuple.getFloatByField("cnn"), pred[0][0]));
+        outputCollector.ack(tuple);
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+        outputFieldsDeclarer.declare(new Fields("url", "cnn", "lstm"));
+    }
+}
---
+++ src/main/java/storm/v4/level1/FinalBolt.java
@@ -0,0 +1,64 @@
+package storm.v4.level1;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseRichBolt;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Values;
+import org.tensorflow.SavedModelBundle;
+import org.tensorflow.Session;
+import org.tensorflow.Tensor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FinalBolt extends BaseRichBolt {
+    private Log log = LogFactory.getLog(FinalBolt.class);
+    private OutputCollector outputCollector;
+    private SavedModelBundle savedModelBundle;
+    private Session sess;
+
+    private float[][] level0Result = new float[1][3];
+
+    private Map<String, Float> cnnMap = new HashMap<>();
+    private Map<String, Float> lstmMap = new HashMap<>();
+    private Map<String, Float> gruMap = new HashMap<>();
+
+    @Override
+    public void prepare(Map map, TopologyContext topologyContext, OutputCollector outputCollector) {
+        this.outputCollector = outputCollector;
+        this.savedModelBundle = SavedModelBundle.load("/home/hjmoon/models/url/","serve");
+        this.sess = savedModelBundle.session();
+    }
+
+    @Override
+    public void execute(Tuple tuple) {
+        String url = tuple.getStringByField("url");
+
+        level0Result[0][0] = tuple.getFloatByField("cnn");
+        level0Result[0][1] = tuple.getFloatByField("lstm");
+        level0Result[0][2] = tuple.getFloatByField("gru");
+
+
+        Tensor x = Tensor.create(level0Result);
+        Tensor result = sess.runner()
+                .feed("final_input/concat:0", x)
+                .fetch("final_output/Sigmoid:0")
+                .run()
+                .get(0);
+
+        float[][] pred = (float[][]) result.copyTo(new float[1][1]);
+
+        outputCollector.emit(new Values(url, pred[0][0]));
+        outputCollector.ack(tuple);
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+        outputFieldsDeclarer.declare(new Fields("url", "pred"));
+    }
+}
