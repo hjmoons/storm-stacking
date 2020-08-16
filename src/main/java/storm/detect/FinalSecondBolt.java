eb720bcb60862441445558cd2fc011ff3f77d465
HJ-log <gywhd408@naver.com>
2020-08-16 22:14:21 +0900

    fix import Preprocessor

--- src/main/java/storm/detect/FinalSecondBolt.java
+++ src/main/java/storm/detect/FinalSecondBolt.java
@@ -14,7 +14,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.tensorflow.SavedModelBundle;
 import org.tensorflow.Session;
 import org.tensorflow.Tensor;
-import storm.old.v2.input.Preprocessor;
+import storm.input.Preprocessor;
 
 import java.io.File;
 import java.io.FileOutputStream;
